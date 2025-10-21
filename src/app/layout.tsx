b363a7ba24b022f20b9c6a89e1d7e409accc9bb9
marcosmrk1 <marcospl.2990@gmail.com>
2025-10-21 11:37:58 -0300

    feat: inicio estilizacao side bar menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,17 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { AppSidebar } from '@/components/Menu/AppSideBar'
+
+const geistSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist-sans',
+})
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+})
 
 export default function RootLayout({
   children,
@@ -9,8 +20,22 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={cn()}>{children}</body>
+    <html lang="pt-BR">
+      <body
+        className={cn(
+          'min-h-screen bg-background text-foreground font-sans antialiased',
+          geistSans.variable,
+          geistMono.variable,
+        )}
+      >
+        <SidebarProvider>
+          <AppSidebar />
+          <main className="p-4">
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
+      </body>
     </html>
   )
 }
---
+++ src/components/Menu/AppSideBar.tsx
@@ -0,0 +1,39 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+import { MENU_ITENS_ROUTES } from '@/routes'
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {MENU_ITENS_ROUTES.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
